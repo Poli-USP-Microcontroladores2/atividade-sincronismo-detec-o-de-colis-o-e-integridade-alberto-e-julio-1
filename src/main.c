f7ca80536f46b02780f73615b49ae1c978b15bc1
AlbertoGNeto <albertogalhegoneto@gmail.com>
2025-11-30 18:26:55 -0300

    Teste 1

--- src/main.c
+++ src/main.c
@@ -5,123 +5,154 @@
 
 #include <string.h>
  
-/* Define UART ports */
-#define UART_TX_NODE DT_NODELABEL(uart0) /* UART for transmitting the echo (to PC) */
-#define UART_RX_NODE DT_NODELABEL(uart1) /* UART for receiving from the other MCU */
-
 /* Use led0 for visual debug feedback */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 #define MSG_SIZE 32
 
-/* queue to store up to 10 messages (aligned to 4-byte boundary) */
-K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
+/* Define UART devices */
+static const struct device *const uart0_dev = DEVICE_DT_GET(DT_NODELABEL(uart0));
+static const struct device *const uart1_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
+
+/* Message queues for passing data between ISRs and threads */
+K_MSGQ_DEFINE(uart0_rx_msgq, MSG_SIZE, 10, 4); // For data from console (UART0)
+K_MSGQ_DEFINE(uart1_rx_msgq, MSG_SIZE, 10, 4); // For data from other MCU (UART1)
 
 static void led_off_work_handler(struct k_work *work)
 {
 	gpio_pin_set_dt(&led, 0); /* Turn LED OFF */
 }
 
-K_WORK_DELAYABLE_DEFINE(led_off_work, led_off_work_handler); //Delay to turn off the led without blocking the interrupt
+K_WORK_DELAYABLE_DEFINE(led_off_work, led_off_work_handler);
 
-static const struct device *const uart_tx_dev = DEVICE_DT_GET(UART_TX_NODE);
-static const struct device *const uart_rx_dev = DEVICE_DT_GET(UART_RX_NODE);
+/* RX buffers and positions for each UART */
+static char rx0_buf[MSG_SIZE];
+static int rx0_buf_pos;
+static char rx1_buf[MSG_SIZE];
+static int rx1_buf_pos;
 
-/* receive buffer used in UART ISR callback */
-static char rx_buf[MSG_SIZE];
-static int rx_buf_pos;
+/* Thread definitions */
+void console_to_uart1_thread(void);
+void uart1_to_console_thread(void);
+
+K_THREAD_DEFINE(console_tid, 1024, console_to_uart1_thread, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(uart1_tid, 1024, uart1_to_console_thread, NULL, NULL, NULL, 7, 0, 0);
 
 /*
- * Read characters from UART until line end is detected. Afterwards push the
- * data to the message queue.
+ * UART0 ISR: handles characters from the console
  */
-void serial_cb(const struct device *dev, void *user_data)
+void serial_cb_uart0(const struct device *dev, void *user_data)
 {
 	uint8_t c;
-
-	if (!uart_irq_update(dev)) {
+	if (!uart_irq_update(dev) || !uart_irq_rx_ready(dev)) {
 		return;
 	}
 
-	if (!uart_irq_rx_ready(dev)) {
-		return;
-	}
-
-	/* read until FIFO empty */
 	while (uart_fifo_read(dev, &c, 1) == 1) {
-		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
-			rx_buf[rx_buf_pos] = '\0';
-
-			/* if queue is full, message is silently dropped */
-			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
-
-			/* Blink the LED without blocking the interrupt */
-			gpio_pin_set_dt(&led, 1); /* Turn LED ON */
-			k_work_reschedule(&led_off_work, K_MSEC(50));
-
-			/* reset the buffer (it was copied to the msgq) */
-			rx_buf_pos = 0;
-		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-			rx_buf[rx_buf_pos++] = c;
+		if ((c == '\n' || c == '\r') && rx0_buf_pos > 0) {
+			rx0_buf[rx0_buf_pos] = '\0';
+			k_msgq_put(&uart0_rx_msgq, &rx0_buf, K_NO_WAIT);
+			rx0_buf_pos = 0;
+		} else if (rx0_buf_pos < (sizeof(rx0_buf) - 1)) {
+			rx0_buf[rx0_buf_pos++] = c;
 		}
-		/* else: characters beyond buffer size are dropped */
 	}
 }
 
 /*
- * Print a null-terminated string character by character to the UART interface
+ * UART1 ISR: handles characters from the other MCU
  */
-void print_uart(char *buf)
+void serial_cb_uart1(const struct device *dev, void *user_data)
 {
-	int msg_len = strlen(buf);
+	uint8_t c;
+	if (!uart_irq_update(dev) || !uart_irq_rx_ready(dev)) {
+		return;
+	}
 
-	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uart_tx_dev, buf[i]);
+	while (uart_fifo_read(dev, &c, 1) == 1) {
+		if ((c == '\n' || c == '\r') && rx1_buf_pos > 0) {
+			rx1_buf[rx1_buf_pos] = '\0';
+			k_msgq_put(&uart1_rx_msgq, &rx1_buf, K_NO_WAIT);
+
+			/* Blink LED to show a message was received */
+			gpio_pin_set_dt(&led, 1); /* Turn LED ON */
+			k_work_reschedule(&led_off_work, K_MSEC(50));
+
+			rx1_buf_pos = 0;
+		} else if (rx1_buf_pos < (sizeof(rx1_buf) - 1)) {
+			rx1_buf[rx1_buf_pos++] = c;
+		}
+	}
+}
+
+/* Thread to send data from console (UART0) to UART1 */
+void console_to_uart1_thread(void)
+{
+	char tx_buf[MSG_SIZE];
+
+	while (k_msgq_get(&uart0_rx_msgq, &tx_buf, K_FOREVER) == 0) {
+		printk("Sending: %s\n", tx_buf);
+		for (int i = 0; i < strlen(tx_buf); i++) {
+			uart_poll_out(uart1_dev, tx_buf[i]);
+		}
+		uart_poll_out(uart1_dev, '\r');
+		uart_poll_out(uart1_dev, '\n');
+	}
+}
+
+/* Thread to print data from UART1 to console (UART0) */
+void uart1_to_console_thread(void)
+{
+	char rx_buf[MSG_SIZE];
+
+	while (k_msgq_get(&uart1_rx_msgq, &rx_buf, K_FOREVER) == 0) {
+		printk("Received: %s\n", rx_buf);
 	}
 }
 
 int main(void)
 {
-	char tx_buf[MSG_SIZE];
-
-	if (!device_is_ready(uart_tx_dev)) {
-		printk("UART TX (UART0) device not found!");
-		return 0;
-	}
-
-	/* --- Visual Debug Check for UART1 --- */
 	if (!gpio_is_ready_dt(&led)) {
 		printk("Debug LED not ready!");
 		return 0;
 	}
 	gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE); /* LED is initially OFF */
 
-	if (!device_is_ready(uart_rx_dev)) {
-		printk("UART RX (UART1) device not found!");
+	/* Check and configure UART0 */
+	if (!device_is_ready(uart0_dev)) {
+		printk("UART0 device not found!");
+		return 0;
+	}
+	/* configure interrupt and callback to receive data from console (UART0) */
+	int ret = uart_irq_callback_user_data_set(uart0_dev, serial_cb_uart0, NULL);
+	if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled for UART0\n");
+		} else if (ret == -ENOSYS) {
+			printk("UART0 device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART0 callback: %d\n", ret);
+		}
+		return 0;
+	}
+	uart_irq_rx_enable(uart0_dev);
+
+	/* Check and configure UART1 */
+	if (!device_is_ready(uart1_dev)) {
+		printk("UART1 device not found!");
 		/* Blink LED rapidly to indicate a fatal error */
 		while (1) {
 			gpio_pin_toggle_dt(&led);
 			k_msleep(100);
 		}
-		return 0; /* This line will not be reached */
 	}
+	IRQ_CONNECT(DT_IRQN(DT_NODELABEL(uart1)), DT_IRQ(DT_NODELABEL(uart1), priority),
+				serial_cb_uart1, DEVICE_DT_GET(DT_NODELABEL(uart1)), 0);
+	irq_enable(DT_IRQN(DT_NODELABEL(uart1)));
+	uart_irq_rx_enable(uart1_dev);
 
-	/* Manually connect our callback to IRQ 13 (UART1) and enable it.
-	 * This bypasses the driver's faulty IRQ_CONNECT mechanism.
-	 */
-	IRQ_CONNECT(DT_IRQN(UART_RX_NODE), DT_IRQ(UART_RX_NODE, priority), serial_cb, DEVICE_DT_GET(UART_RX_NODE), 0);
-	irq_enable(DT_IRQN(UART_RX_NODE));
-	uart_irq_rx_enable(uart_rx_dev);
-
-	print_uart("UART bridge started. Listening on UART1, echoing to UART0.\r\n");
-
-	/* indefinitely wait for input from the user */
-	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
-		print_uart("Received:");
-		print_uart(tx_buf);
-		print_uart("\n");
-	}
+	printk("MCU-to-MCU Chat Application Started\n");
+	printk("Type a message and press Enter to send.\n");
 
 	return 0;
 }
